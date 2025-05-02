7ddad5479599a757751e845af1bfcd4d6470c2db
Flurop <flora@ratgers.nl>
2025-05-02 10:06:00 +0200

    Updated get-public-key.js

--- netlify/functions/get-public-key.js
+++ netlify/functions/get-public-key.js
@@ -1,8 +1,23 @@
 exports.handler = async () => {
-  return {
-    statusCode: 200,
-    body: JSON.stringify({
-      key: process.env.STRIPE_PUBLIC_KEY,
-    }),
-  };
+  try {
+    const publishableKey = process.env.STRIPE_PUBLISHABLE_KEY;
+    
+    if (!publishableKey) {
+      throw new Error('Missing STRIPE_PUBLISHABLE_KEY environment variable');
+    }
+
+    return {
+      statusCode: 200,
+      body: JSON.stringify({
+        key: publishableKey,
+      }),
+    };
+  } catch (error) {
+    return {
+      statusCode: 500,
+      body: JSON.stringify({
+        error: error.message,
+      }),
+    };
+  }
 };
